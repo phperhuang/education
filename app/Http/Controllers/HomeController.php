fe0ee1b8bcc6e5d2076a80d1e322253a01eff190
Hx <1020309534@qq.com>
2019-04-30 16:40:54 +0800

    update HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -25,4 +26,10 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function getGuard()
+    {
+        return Auth::guard();
+    }
+
 }
