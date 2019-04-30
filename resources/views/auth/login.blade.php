4b10a8ec9e3250c021d7d001d5ed54956ab113ac
Hx <1020309534@qq.com>
2019-04-30 10:33:24 +0800

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('email') is-invalid @enderror" name="name" value="{{ old('name') }}" required autofocus>
 
-                                @error('email')
+                                @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
